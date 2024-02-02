f5d0e3be8be8c9213a46a3c6a38d8bf0f2cd418e
adam <apazner@wisc.edu>
2024-02-02 12:29:01 -0600

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Adam on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
